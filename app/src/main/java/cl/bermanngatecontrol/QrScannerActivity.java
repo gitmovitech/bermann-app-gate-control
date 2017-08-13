2e167639a4846488d62d777af3b5faade2b26734
Victor Vargas <vvargas@movitech.cl>
2017-08-13 12:35:14 -0300

    inicializacion

---
+++ app/src/main/java/cl/bermanngatecontrol/QrScannerActivity.java
@@ -0,0 +1,13 @@
+package cl.bermanngatecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QrScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+    }
+}
