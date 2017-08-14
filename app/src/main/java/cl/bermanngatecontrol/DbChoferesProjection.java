1cf23121a660e65c44eba373986735b070391d2c
Victor Vargas <vvargas@movitech.cl>
2017-08-14 13:38:38 -0300

    estructura de base de datos

---
+++ app/src/main/java/cl/bermanngatecontrol/DbChoferesProjection.java
@@ -0,0 +1,17 @@
+package cl.bermanngatecontrol;
+
+
+import android.provider.BaseColumns;
+
+public class DbChoferesProjection {
+    public static abstract class Entry implements BaseColumns {
+        public static final String TABLE_NAME ="choferes";
+
+        public static final String ID = "ID";
+        public static final String RUT = "RUT";
+        public static final String NOMBRE = "NOMBRE";
+        public static final String APELLIDO_PATERNO = "APELLIDO_PATERNO";
+        public static final String ESTADO = "ESTADO";
+        public static final String FOTO = "FOTO";
+    }
+}
